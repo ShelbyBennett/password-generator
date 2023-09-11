6c2f1573535392cfd3d031910ae3fc129e1726e8
Shelby Bennett <shelshel@Shelbys-MBP.lan>
2023-09-11 09:32:20 -0400

    added variables, arrays, while loop, generate password function

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Assignment code here
+var length=0
+var userchoices= []
+var result= ""
+var uppercase= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers= [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialchar= [ "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", " ' ", " ", ' " ', "\\" ];
 
-
+function generatePassword (){
+  while(length <8 ||length >128 || isNaN(length) || length==="" ){
+    length= prompt ("please enter a password between 8 and 128 characters")
+  }
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
